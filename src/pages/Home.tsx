4e59428959210879605b5e066765de5ab9243853
choongnyeong6215 <choong6215@gmail.com>
2024-04-04 22:52:07 +0900

    홈화면 썸네일 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,35 @@
+import { useQuery } from "react-query";
+import { getNowPlayingMovies } from "../api";
+import { INowPlayingMovies } from "../interfaces/nowPlayingMvInterface";
+import { Thumbnail, Overview, Title, Wrapper } from "../styles/homeStyle";
+import { ClipLoader } from "react-spinners";
+import { makeThumbnailPath } from "../makeThumbnail";
+
 const Home = () => {
-  return <div>Home</div>;
+  // 현재 상영중인 영화
+  const { data, isLoading } = useQuery<INowPlayingMovies>({
+    queryKey: ["nowPlayingMovie"],
+    queryFn: getNowPlayingMovies,
+  });
+
+  return (
+    <Wrapper>
+      {isLoading ? (
+        <ClipLoader color="#E50915" loading={isLoading} size={150} />
+      ) : (
+        <>
+          <Thumbnail
+            $backdropPath={makeThumbnailPath(
+              data?.results[0].backdrop_path || ""
+            )}
+          >
+            <Title>{data?.results[0].title}</Title>
+            <Overview>{data?.results[0].overview}</Overview>
+          </Thumbnail>
+        </>
+      )}
+    </Wrapper>
+  );
 };
 
 export default Home;
---
+++ src/styles/homeStyle.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  background-color: black;
+  height: 100vh;
+`;
+
+export const Thumbnail = styled.div<{ $backdropPath: string }>`
+  height: 200vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  color: white;
+  padding: 60px;
+  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
+    url(${(props) => props.$backdropPath});
+  background-size: cover;
+`;
+
+export const Title = styled.h2`
+  font-size: 5rem;
+  margin-bottom: 1rem;
+`;
+
+export const Overview = styled.p`
+  font-size: 2rem;
+  width: 50%;
+`;
